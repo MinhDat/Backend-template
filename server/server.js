c6309b6221deb200e243156bc9de59b141edd86e
haminhdat <dat.ha@vexere.com>
2018-07-20 19:28:32 +0700

    update index.js v4

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use("/api", API);
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 app.set("port", port);
 /**
  * Create HTTP server.
